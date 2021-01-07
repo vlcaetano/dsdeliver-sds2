5741c441cd0bf14e57e19db321ba548bf97a96cb
Vitor Caetano <vitorlc95@gmail.com>
2021-01-07 10:55:16 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 // import React from 'react'; não é mais necessário
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
