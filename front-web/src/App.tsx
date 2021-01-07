14bc898cbadf5388ca75d63565165309265e52b4
Vitor Caetano <vitorlc95@gmail.com>
2021-01-07 09:32:22 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'; não é mais necessário
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
